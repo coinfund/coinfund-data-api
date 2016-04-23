64e65653fb164baa739e41d5ee93de9850308380
Jake Brukhman <jbrukh@gmail.com>
2016-04-23 19:48:47 -0400

    projects show index

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,11 +1,23 @@
 class Api::ProjectsController < ApplicationController
 
+  # before_action :get_resource, only: [:show]
+
   def index
     @projects = Coinfund::Project.all
   end
 
   def show
     @project = Coinfund::Project.find(params[:id])
+    puts 'what the what?!', @project
   end
+
+  private
+
+    def get_resource
+      @project = Coinfund::Project.find(params[:id])
+      if !@project
+        head :not_found
+      end
+    end
 
 end
