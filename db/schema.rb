c2d515e53cddb394a8100a28bf31b5f0e4a1c213
Jake Brukhman <jbrukh@gmail.com>
2016-05-23 17:02:58 -0400

    Save.

--- db/schema.rb
+++ db/schema.rb
@@ -13,24 +13,29 @@
 
 ActiveRecord::Schema.define(version: 20160421051607) do
 
-  create_table "coinfund_projects", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.string   "homepage"
     t.integer  "status",     default: 0
+    t.boolean  "watching",   default: false
+    t.boolean  "engagement", default: false
+    t.boolean  "long",       default: false
     t.integer  "sector_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.index ["sector_id"], name: "index_projects_on_sector_id", using: :btree
   end
 
-  add_index "coinfund_projects", ["sector_id"], name: "index_coinfund_projects_on_sector_id"
-
-  create_table "coinfund_sectors", force: :cascade do |t|
+  create_table "sectors", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "coinfund_users", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "first_name",      null: false
     t.string   "last_name",       null: false
     t.string   "email"
@@ -40,4 +45,5 @@ ActiveRecord::Schema.define(version: 20160421051607) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "projects", "sectors"
 end
